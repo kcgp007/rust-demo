351c5b8b5b9652c7470a24b404a5b55c9ebd94a7
金 <>
2022-10-20 17:40:34 +0800

    拆分 tch_demo & tf_demo

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 mod logging;
+mod tch_demo;
+mod tf_demo;
 
 use tch::nn::{Module, OptimizerConfig};
 use tch::{nn, Device};
@@ -13,40 +15,5 @@ fn main() {
 
     error!("error");
 
-    // run().unwrap();
+    tch_demo::run().unwrap();
 }
-
-// fn net(vs: &nn::Path) -> impl Module {
-//     nn::seq()
-//         .add(nn::linear(
-//             vs / "layer1",
-//             IMAGE_DIM,
-//             HIDDEN_NODES,
-//             Default::default(),
-//         ))
-//         .add_fn(|xs| xs.relu())
-//         .add(nn::linear(vs, HIDDEN_NODES, LABELS, Default::default()))
-// }
-
-// fn run() -> Result<()> {
-//     let m = tch::vision::mnist::load_dir("data")?;
-//     let vs = nn::VarStore::new(Device::Cpu);
-//     let net = net(&vs.root());
-//     let mut opt = nn::Adam::default().build(&vs, 1e-3)?;
-//     for epoch in 1..200 {
-//         let loss = net
-//             .forward(&m.train_images)
-//             .cross_entropy_for_logits(&m.test_labels);
-//         opt.backward_step(&loss);
-//         let test_accuracy = net
-//             .forward(&m.test_images)
-//             .accuracy_for_logits(&m.test_labels);
-//         println!(
-//             "epoch: {:4} train loss: {:8.5} test acc: {:5.2}%",
-//             epoch,
-//             f64::from(&loss),
-//             100. * f64::from(&test_accuracy),
-//         );
-//     }
-//     Ok(())
-// }
---
+++ src/tch_demo.rs
@@ -0,0 +1,38 @@
+use std::error::Error;
+use tch::nn::{Module, OptimizerConfig};
+use tch::{nn, Device};
+
+fn net(vs: &nn::Path) -> impl Module {
+    nn::seq()
+        .add(nn::linear(
+            vs / "layer1",
+            IMAGE_DIM,
+            HIDDEN_NODES,
+            Default::default(),
+        ))
+        .add_fn(|xs| xs.relu())
+        .add(nn::linear(vs, HIDDEN_NODES, LABELS, Default::default()))
+}
+
+pub fn run() -> Result<(), Box<dyn Error>> {
+    let m = tch::vision::mnist::load_dir("data")?;
+    let vs = nn::VarStore::new(Device::Cpu);
+    let net = net(&vs.root());
+    let mut opt = nn::Adam::default().build(&vs, 1e-3)?;
+    for epoch in 1..200 {
+        let loss = net
+            .forward(&m.train_images)
+            .cross_entropy_for_logits(&m.test_labels);
+        opt.backward_step(&loss);
+        let test_accuracy = net
+            .forward(&m.test_images)
+            .accuracy_for_logits(&m.test_labels);
+        println!(
+            "epoch: {:4} train loss: {:8.5} test acc: {:5.2}%",
+            epoch,
+            f64::from(&loss),
+            100. * f64::from(&test_accuracy),
+        );
+    }
+    Ok(())
+}
