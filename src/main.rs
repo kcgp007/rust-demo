cad1200a66c9688d405e17223a1a229ace15ffcf
金 <>
2022-10-26 18:09:12 +0800

    Rust语言圣经

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,12 @@
+mod demo;
 mod logging;
-mod tch_demo;
-mod tf_demo;
-
-use tch::nn::{Module, OptimizerConfig};
-use tch::{nn, Device};
-use tracing::{error, info, trace};
 
 fn main() {
     logging::init();
 
-    info!("aaa");
+    // info!("aaa");
+    // trace!("error");
+    // error!("error");
 
-    trace!("error");
-
-    error!("error");
-
-    tch_demo::run().unwrap();
+    demo::main();
 }
