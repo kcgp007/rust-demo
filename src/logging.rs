d3b1cf6a8ff6532ca5b8645be504f45d0b851921
金 <>
2024-04-23 03:58:04 +0800

    tracing使用chrono时间库

--- src/logging.rs
+++ src/logging.rs
@@ -1,24 +1,27 @@
 use std::io;
+use tracing::Level;
 use tracing_appender::rolling;
 use tracing_error::ErrorLayer;
 use tracing_subscriber::fmt::time;
+use tracing_subscriber::fmt::writer::MakeWriterExt;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{fmt, Registry};
 
 pub fn init() {
+    let lv = Level::DEBUG;
     // 输出到stdout
     let subscriber = fmt::layer()
-        .with_timer(time::LocalTime::rfc_3339())
+        .with_timer(time::ChronoLocal::rfc_3339())
         .with_thread_ids(true)
         .with_thread_names(true)
-        .with_writer(io::stdout)
+        .with_writer(io::stdout.with_max_level(lv))
         .pretty();
 
     // 输出到文件
     let file_subscriber = fmt::layer()
         .with_ansi(false)
-        .with_timer(time::LocalTime::rfc_3339())
+        .with_timer(time::ChronoLocal::rfc_3339())
         .with_thread_ids(true)
         .with_thread_names(true)
         .with_writer(rolling::daily("log", "demo.log"))
